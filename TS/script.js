29e3e8c1d75d6228ef1bdbcb146a43509ed7b7c7
pravincoder1 <ypravin921@gmail.com>
2024-02-12 15:07:44 +0530

    First App

---
+++ TS/script.js
@@ -0,0 +1,6 @@
+var a = 20;
+var rno;
+var k = "steve";
+console.log(typeof a, typeof rno, typeof k);
+k = 25;
+console.log(typeof k);
---
+++ TS/script.ts
@@ -0,0 +1,6 @@
+let a: number = 20;
+let rno;
+let k:number|string = "steve"
+console.log(typeof a, typeof rno, typeof k)
+k = 25;
+console.log(typeof k)
